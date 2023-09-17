ad10eb11d16f4affa551dd557c83de9df997e75e
yaoc1996 <yaoc1996@gmail.com>
2023-09-16 21:23:42 -0400

    added unix install scripts

---
+++ install-eigen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir eigen
+cd eigen
+curl https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip --output eigen.zip
+tar -xf eigen.zip
+cd eigen-3.4.0
+mkdir build
+cd build
+cmake ..
+cmake --build . --target install
+cd ..
+cd ..
+cd ..
+rm -r eigen
